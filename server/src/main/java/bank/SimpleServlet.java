40ad6750de6ff8b1093766403939774023ba48cd
hoangviet.nguyen <hoangviet.nguyen@students.fhnw.ch>
2026-02-10 16:58:22 +0100

    implemented a simple tomcat server

---
+++ server/src/main/java/bank/SimpleServlet.java
@@ -0,0 +1,23 @@
+package bank;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/simple/*")
+public class SimpleServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><title>Welcome</title><body>");
+        writer.println("<h1>Have a Great Day!</h1>" + LocalDateTime.now());
+        writer.println("</body></html>");
+    }
+}
