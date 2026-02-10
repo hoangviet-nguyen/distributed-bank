33562683651322e497c0e4c4ac42da871a0c811f
hoangviet.nguyen <hoangviet.nguyen@students.fhnw.ch>
2026-02-10 17:25:48 +0100

    moved servlet to own file

---
+++ server/src/main/java/bank/servlet/ServerBank.java
@@ -0,0 +1,37 @@
+package bank.servlet;
+
+
+import java.io.File;
+
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+
+public class ServerBank {
+    public static void main( String[] args ) throws Exception {
+        
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8081);
+        tomcat.setBaseDir("target");
+
+        String webAppDir = "src/main/webapp";;
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("", new File(webAppDir).getAbsolutePath());
+
+        File classFiles = new File("target/classes/");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(
+            new DirResourceSet(
+                resources,
+                "/WEB-INF/classes",
+                classFiles.getAbsolutePath(),
+                "/"
+        ));
+        ctx.setResources(resources);
+        tomcat.getConnector(); // creates the default connector
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
